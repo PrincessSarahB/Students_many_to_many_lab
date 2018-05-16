4f75cb23c3c5bf1ad4b55e3b7258c9dc6a313623
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-16 14:05:17 +0100

    initial commit, course class set up

---
+++ src/main/java/models/Course.java
@@ -0,0 +1,74 @@
+package models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="courses")
+public class Course {
+    private int id;
+    private String title;
+    private String level;
+    private Set<Lesson> lessons;
+    private Set<Student> students;
+
+    public Course() {
+    }
+
+    public Course(String title, String level) {
+        this.title = title;
+        this.level = level;
+        this.students = new HashSet<Student>();
+        this.lessons = new HashSet<Lesson>();
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name="level")
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    @OneToMany(mappedBy = "course")
+    public Set<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(Set<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="course_id", nullable = false)
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}

01a247d6e0b25a36c2129cc589146372e4376d29
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-16 14:23:49 +0100

    Create lesson class

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,7 +1,75 @@
 package models;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Lesson {
 
+    private int id;
+    private String title;
+    private int roomNumber;
+    private Course course;
+    private Set<Student> students;
+
+    public Lesson() {
+    }
+
+    public Lesson(String title, int roomNumber, Course course) {
+        this.title = title;
+        this.roomNumber = roomNumber;
+        this.course = course;
+        this.students = new HashSet<Student>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Column(name = "room_number")
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="course_id", nullable = false)
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "student_lessons",
+            joinColumns = {@JoinColumn(name = "lesson_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "student_id", nullable = false, updatable = false)}
+    )
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
 }
